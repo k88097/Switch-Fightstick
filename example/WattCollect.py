37dbfad4700dc00c4d3e52bfcdd8a755efe12e1a
PinYuan <d59606110@gmail>
2020-03-17 21:20:58 +0800

    修正BUG

--- example/WattCollect.py
+++ example/WattCollect.py
@@ -3,7 +3,7 @@ import time
 
 
 # 瓦特收集
-def WattCollect(year, month, day):
+def WattCollect():
     # 站在洞前
     # ctr.A()
     # time.sleep(0.5)
@@ -39,33 +39,42 @@ def WattCollect(year, month, day):
     ctr.d()
     ctr.A()
 
-    if day == max_day[month - 1]:
-        if month == 12:
-            NextYear()
-            year += 1
-            month = 1
-        else:
-            NextMonth()
-            month += 1
-        day = 1
-    else:
-        # 一般模式
-        NTime()
+    #調整時間加一天
+    ctr.r()
+    ctr.r()
+    ctr.u()
+    ctr.r()
+    ctr.r()
+    ctr.r()
+    ctr.A()
+
+    # if day == max_day[month - 1]:
+    #     if month == 12:
+    #         NextYear()
+    #         year += 1
+    #         month = 1
+    #     else:
+    #         NextMonth()
+    #         month += 1
+    #     day = 1
+    # else:
+    #     # 一般模式
+    #     NTime()
 
     # 回到主畫面並且進入遊戲
-    print("回到主畫面並且進入遊戲")
+    # print("回到主畫面並且進入遊戲")
     ctr.h()
     time.sleep(1)
     ctr.h()
     time.sleep(1)
 
     # 退出招募
-    print("退出招募")
+    # print("退出招募")
     ctr.A()
     time.sleep(4.3)
 
     # 領取瓦特
-    print("領取瓦特")
+    # print("領取瓦特")
     ctr.A()
     time.sleep(0.3)
     ctr.A()
@@ -109,10 +118,6 @@ def NextYear():
 
 if __name__ == "__main__":
     ctr = Controller()
-    global year, month, day
-    year, month, day = [
-        int(i) for i in input("輸入目前日期\n按照此格式輸入2020.3.17 : ").split(".")
-    ]
 
     print("{}搜尋控制器{}".format("=" * 5, "=" * 5))
     ctr.LR()
@@ -120,9 +125,5 @@ if __name__ == "__main__":
 
     print("{}開始執行程式{}".format("=" * 5, "=" * 5))
     while (True):
-        global max_day
-        max_day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        if year % 4 == 0:
-            max_day[1] = 29
-        WattCollect(year, month, day)
+        WattCollect()
     ctr.close()
