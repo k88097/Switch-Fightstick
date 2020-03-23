247884073c325be991871350f7c06dcc319b38fb
PinYuan <d59606110@gmail.com>
2020-03-23 12:52:58 +0800

    優化換日腳本

--- example/Change_day.py
+++ example/Change_day.py
@@ -1,44 +1,68 @@
 from NXController import Controller
-import time
+import time, datetime
 
 
 # 過日
-def change_day(isSaved):
-    c = 0
+def change_day(ctr, isSaved, current_date):
+    tommorrow = current_date + datetime.timedelta(days=1)
 
     if isSaved:
-        c += 1
-        # 第一次過日
+        # 第一次進入點選時間
         # 過日，點選時間
         ctr.A()
 
-        #跳到 "日"，並且加一天
-        ctr.r(0.05)
-        ctr.r(0.05)
+        # 當年的最後一天
+        if current_date.month == 12 and current_date.day == 31:
+            #加 年月日
+            ctr.u(0.05)
+            ctr.r(0.05)
+            ctr.u(0.05)
+            ctr.r(0.05)
+            ctr.u(0.05)
+        elif not current_date.month == tommorrow.month:
+            #加 月日
+            ctr.r(0.05)
+            ctr.u(0.05)
+            ctr.r(0.05)
+            ctr.u(0.05)
+        else:
+            #跳到 "日"，並且加一天
+            ctr.r(0.05)
+            ctr.r(0.05)
+            ctr.u(0.05)
+
+            isSaved = False
+
+        ctr.A(0.05)
+        ctr.A(0.05)
+        ctr.A(0.05)
+        ctr.A()
+
+    else:
+        # 點選時間
+        ctr.A()
+
+        for i in range(3):
+            ctr.l(0.05)
+
+        if current_date.month == 12 and current_date.day == 31:
+            for i in range(2):
+                ctr.l(0.05)
+            for i in range(2):
+                ctr.u(0.05)
+                ctr.r(0.05)
+        elif not current_date.month == tommorrow.month:
+            ctr.l(0.05)
+            ctr.u(0.05)
+            ctr.r(0.05)
+
         ctr.u(0.05)
         ctr.A(0.05)
         ctr.A(0.05)
         ctr.A(0.05)
         ctr.A()
 
-        isSaved = False
-        # time.sleep(0.3)
-
-    while c < 31:
-        c += 1
-        # 快速過日
-        ctr.A()
-        # time.sleep(0.1)
-        ctr.l(0.05)
-        ctr.l(0.05)
-        ctr.l(0.05)
-        ctr.u(0.05)
-        ctr.A(0.05)
-        ctr.A(0.05)
-        ctr.A(0.05)
-        ctr.A()
-        # time.sleep(0.3)
-    return isSaved
+    return isSaved, tommorrow
 
 
 # 自動存檔
@@ -90,13 +114,16 @@ def save(isSaved):
 if __name__ == "__main__":
     ctr = Controller()
 
-    goal = int(input("輸入天數："))
-    count = 0
-    isSaved = True
-
     print("{}搜尋控制器{}".format("=" * 5, "=" * 5))
     ctr.LR()
-    time.sleep(2)
+
+    year, month, day = [
+        int(i) for i in input("輸入目前日期 (YYYY.MM.DD):").split('.')
+    ]
+    count, goal = 0, int(input("輸入天數："))
+    # 當前日期轉為datetime格式
+    current_date = datetime.datetime(year, month, day)
+    isSaved = True
 
     print("{}開始執行程式{}".format("=" * 5, "=" * 5))
     print("[%s] 刷時間中..." % time.strftime("%H:%M:%S", time.localtime()))
@@ -104,17 +131,17 @@ if __name__ == "__main__":
     # isSaved = save(isSaved)
 
     while count < goal:
-        count += 30
+        count += 1
 
-        isSaved = change_day(isSaved)
-        if not count == goal:
+        isSaved, current_date = change_day(ctr, isSaved, current_date)
+        if count % 30 == 0 and not count == goal:
             print("[%s] 還剩下 %d 天。" %
                   (time.strftime("%H:%M:%S", time.localtime()),
                    (goal - count)))
 
         # 每300天回去存檔一次
         if count % 300 == 0:
-            time.sleep(1.5)
+            time.sleep(1)
             isSaved = save(isSaved)
 
     print("[%s] 已達目標天數。" % time.strftime("%H:%M:%S", time.localtime()))
