3ab2c98270e2d6f2e3a3b32c825a8f9d127bdeb9
PinYuan <d59606110@gmail>
2020-03-26 23:28:25 +0800

    優化 瓦特領取

--- example/WattCollect.py
+++ example/WattCollect.py
@@ -1,9 +1,12 @@
 from NXController import Controller
 import time
+import datetime
 
 
 # 瓦特收集
-def WattCollect(count):
+def WattCollect(ctr, current_date):
+    tommorrow = current_date + datetime.timedelta(days=1)
+
     # 站在洞前
     ctr.A()
     time.sleep(2)
@@ -33,16 +36,28 @@ def WattCollect(count):
     ctr.A()
 
     #到調整時間選項
-    ctr.d(0.6)
+    ctr.d(0.05)
+    ctr.d(0.05)
     ctr.A()
 
-    #調整時間加一天
-    ctr.r(0.05)
-    ctr.r(0.05)
-    ctr.u(0.05)
+    # 調整時間加一天
+    if current_date.month == 12 and current_date.day == 31:
+        for i in range(3):
+            ctr.u(0.05)
+            ctr.A(0.05)
+    elif not current_date.month == tommorrow.month:
+        ctr.A(0.05)
+        for i in range(2):
+            ctr.u(0.05)
+            ctr.A(0.05)
+    else:
+        ctr.A(0.05)
+        ctr.A(0.05)
+        ctr.u(0.05)
     ctr.A(0.05)
     ctr.A(0.05)
     ctr.A(0.05)
+    # 按下確定
     ctr.A()
 
     # 回到主畫面並且進入遊戲
@@ -66,6 +81,7 @@ def WattCollect(count):
     ctr.A()
     time.sleep(0.4)
     print("[%d] 領取 2000w" % count)
+    return tommorrow
 
 
 # 存檔
@@ -90,13 +106,18 @@ if __name__ == "__main__":
     print("{}搜尋控制器{}".format("=" * 5, "=" * 5))
     ctr.LR()
 
-    goal = int(input("輸入次數："))
-    count = 0
+    year, month, day = [
+        int(i) for i in input("輸入目前日期 (YYYY.MM.DD):").split('.')
+    ]
+    count, goal = 0, int(input("輸入次數："))
+    # 當前日期轉為datetime格式
+    current_date = datetime.datetime(year, month, day)
+    isSaved = True
 
     print("{}開始執行程式{}".format("=" * 5, "=" * 5))
-    while (count < goal):
+    while count < goal:
         count += 1
-        WattCollect(count)
+        current_date = WattCollect(ctr, current_date)
 
         if count % 100 == 0:
             time.sleep(1.8)
